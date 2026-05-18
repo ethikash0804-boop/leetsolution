7be30c4ce3143c2febd86c37019a5952439d5ad1
ethikash0804-boop <ethikash.0804@gmail.com>
2026-05-18 22:16:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        one = 2
+        two = 1
+
+        for i in range(3, n + 1):
+            temp = one + two
+            two = one
+            one = temp
+
+        return one
